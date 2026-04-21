aa0927b52f100436410a1aaf368ba24d3133eb28
iviuho <iviuho@gmail.com>
2026-04-21 23:32:51 +0900

    ✨ Add header menu

--- app/header.tsx
+++ app/header.tsx
@@ -1,12 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const menus = [{ link: "/message", text: "메시지" }];
 
 function Logo() {
   return (
     <Button size="lg" variant="ghost" asChild>
       <Link href="/">
-        <Image alt="bonfire logo" src="/bonfire_logo.svg" width={30} height={30} />
+        <Image alt="bonfire logo" src="/bonfire_logo.svg" width={30} height={30} loading="eager" />
         bonfire
       </Link>
     </Button>
@@ -14,9 +24,21 @@ function Logo() {
 }
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <div className="flex items-center min-h-15 p-4 gap-4">
       <Logo />
+
+      <NavigationMenu>
+        {menus.map((menu, index) => (
+          <NavigationMenuItem key={index}>
+            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()} data-active={pathname === menu.link}>
+              <Link href={menu.link}>{menu.text}</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenu>
     </div>
   );
 }
